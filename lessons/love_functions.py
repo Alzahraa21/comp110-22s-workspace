00e22131150139ebb4f648c4e0d9298a7ef2db66
Alzahraa Al Bayati <alzahraa@live.unc.edu>
2022-02-06 20:22:11 -0500

    saving

---
+++ lessons/love_functions.py
@@ -0,0 +1,16 @@
+"""Some examples of tender, loving functions"""
+
+def love(name: str) -> str: 
+    """Given a name as a parameter, returns a loving string"""
+    return f"I love you {name} !"
+
+
+def spread_love(to: str, n: int) -> str:
+    """Generates a string that repeats a loving message"""
+    love_note: str = " "
+    counter: int = 0
+    while counter < n:
+        counter += 1
+        love_note = love_note + love( to ) +  "\n" 
+    return love_note
+
